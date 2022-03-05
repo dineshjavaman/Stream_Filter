bd4416fd88cba25120721109e26e026139a994ef
dinesh murugesan <dineshjavaman@gmail.com>
2022-03-05 21:46:53 +0530

    Add files via upload

--- stream-api-filter/src/com/stream/filter/controller/PersonController.java
+++ stream-api-filter/src/com/stream/filter/controller/PersonController.java
@@ -1,8 +1,52 @@
 package com.stream.filter.controller;
 
+import com.stream.filter.model.Person;
+import com.stream.filter.service.PersonService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class PersonController {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
+        PersonService personService = new PersonService();
+
+        ArrayList<Person> persons = personService.getPersons();
+        {
+            //to find person live in madurai
+        List<Person> perssoninmadurai = persons.stream()
+                .filter(x -> "Madurai".equalsIgnoreCase(x.getAddress().getCity()))
+                .collect(Collectors.toList());
+            System.out.println("The persons lives in madurai");
+        perssoninmadurai.forEach(x-> System.out.println(x.getName()));
+            }
+            // persons studying EEE in the list
+        {
+            List<Person> personstudynigEEE = persons.stream()
+                    .filter(x -> "EEE".equalsIgnoreCase(x.getAcademy().getDept()))
+                    .collect(Collectors.toList());
+            System.out.println("The persons studying");
+            personstudynigEEE.forEach(x-> System.out.println( x.getName()));
+        }
+
+        //THE LIST OF DEPARTMENT NAME WHOSE SEX IS M
+        {
+            List<Person> departmentname = persons.stream()
+                    .filter(x -> "m".equalsIgnoreCase(x.getSex()))
+                    .collect(Collectors.toList());
+            System.out.println("THE LIST OF DEPARTMENT NAME WHOSE SEX IS M");
+            departmentname.forEach(x-> System.out.println(x.getName() +"-"+ x.getAcademy().getDept()));
+        }
+        //the list of person name whose is F
+        {
+            List<Person>Personname = persons.stream()
+                    .filter(x -> "f".equalsIgnoreCase(x.getSex()))
+                    .collect(Collectors.toList());
+            System.out.println("THE LIST OF DEPARTMENT NAME WHOSE SEX IS f");
+            Personname.forEach(x-> System.out.println(x.getName()));
+        }
+System.out.println("diensh");
+
+}}
--- stream-api-filter/src/com/stream/filter/model/Address.java
+++ stream-api-filter/src/com/stream/filter/model/Address.java
@@ -5,19 +5,19 @@ public class Address {
     String city;
     String state;
 
-    public String getCity() {
-        return city;
-    }
-
     public void setCity(String city) {
         this.city = city;
     }
 
-    public String getState() {
-        return state;
-    }
-
     public void setState(String state) {
         this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
     }
 }
